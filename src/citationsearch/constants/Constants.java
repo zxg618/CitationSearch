11cc6b7e72aca0cda8eee9fb6f4ebdde54b909cf
zxg618 <zxg618@gmail.com>
2017-06-18 02:01:11 +1000

    v1.0

--- src/citationsearch/constants/Constants.java
+++ src/citationsearch/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
 	//public static final String FILE_PATH = "/Users/zxg/Documents/workspace/CitationSearch/patentfirmsmandenglish.xlsx";
 	//public static final String FILE_PATH = "/Users/zxg/Documents/workspace/CitationSearch/patentfirmsmandenglish_test.xlsx";
 	//public static final String FILE_PATH = "/Users/zxg/Documents/workspace/CitationSearch/patentfirmsmandenglish_test2.xlsx";
-	public static final String FILE_PATH = "/Users/zxg/Documents/workspace/CitationSearch/patentfirmsmandenglish_test2.xlsx";
+	public static final String FILE_PATH = "/Users/zxg/Documents/workspace/CitationSearch/patentfirmsmandenglish_test3.xlsx";
 	public static final String LOG_IN_URL = "https://patentscope.wipo.int/search/en/reg/login.jsf";
 	public static final String DATA_FILE_POSTFIX = ".out";
 	public static final String PSN_SECTOR_COMPANY = "COMPANY";
